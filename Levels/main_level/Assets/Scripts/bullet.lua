e6e8f33dd9d2ee81ead5c7f29f8650522b045827
LB-TomaszSerwanski <v-tomasz.serwanski@lionbridge.com>
2021-07-28 15:53:30 +0200

    second commit

--- Levels/main_level/Assets/Scripts/bullet.lua
+++ Levels/main_level/Assets/Scripts/bullet.lua
@@ -2,36 +2,30 @@ local bullet =
 {
 	Properties = 
 	{
-		Speed = {default = 30}
+		Speed = {default = 30};
 	}
 }
 
 function bullet:OnActivate()
-	self.tickHandler = TickBus.Connect(self)
 	self.Speed = self.Properties.Speed
 	self.forward = EntityTransform_VM.GetEntityRight(self.entityId, 1)
-	
---[[
-	self.collisionCount = 0
 	
 --________________________________________________TRIGGER_____________________________________________
 	self.triggerEnterBusId = SimulatedBody.GetOnTriggerEnterEvent(self.entityId);
 	self.triggerEnterBus = self.triggerEnterBusId:Connect(
-		function(...)
-			self.collisionCount = self.collisionCount + 1
-			log("Bullet Entered Trigger " .. tostring(self.collisionCount))
-			
-		end)
-
-	self.triggerExitBusId = SimulatedBody.GetOnTriggerExitEvent(self.entityId);
-	self.triggerExitBus = self.triggerExitBusId:Connect(
-		function(...)
-			self.collisionCount = self.collisionCount - 1
-			log(" Bullet Exited Trigger " .. tostring(self.collisionCount))
+		function(x, y)
+			self.children = TransformBus.Event.GetChildren(self.entityId)
+			for i = 0,#self.children,1 do
+				GameEntityContextRequestBus.Broadcast.DeactivateGameEntity(self.children[i])
+			end
+			GameEntityContextRequestBus.Broadcast.DeactivateGameEntity(self.entityId)
+			self.body1 = TriggerEvent.GetOtherEntityId(y)
+			HitSE.Event.ReceiveHit(self.body1, 40)
 		end)
 --@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 --]]
 
+	self.tickHandler = TickBus.Connect(self)
 end
 
 function bullet:OnDeactivate()
@@ -40,10 +34,7 @@ end
 
 function bullet:OnTick(dt, stp)
 	TransformBus.Event.RotateAroundLocalX(self.entityId, 30*dt)
-	
---	self.forward = EntityTransform_VM.GetEntityForward(self.entityId, 1)
-	TransformBus.Event.MoveEntity(self.entityId,self.forward * self.Speed * dt) -- Vector3(self.Speed * dt,0,0)
-	
+	TransformBus.Event.MoveEntity(self.entityId,self.forward * self.Speed * dt)
 end
 
 return bullet
