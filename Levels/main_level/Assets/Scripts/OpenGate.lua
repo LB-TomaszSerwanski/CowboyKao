7d2736e1adce1fda0a51dc2f54300d44c2e68427
LB-TomaszSerwanski <v-tomasz.serwanski@lionbridge.com>
2021-07-30 10:16:51 +0200

    Scripted main, storyline, hints UI

--- Levels/main_level/Assets/Scripts/OpenGate.lua
+++ Levels/main_level/Assets/Scripts/OpenGate.lua
@@ -5,12 +5,14 @@ local unlockBar =
 function unlockBar:OnActivate()
 	self.ScriptEventHandler = HitSE.Connect(self, self.entityId)
 	self.health = 2
+	self.eventReceived = false
 end
 
 function unlockBar:ReceiveHit(entityId, damage)
 	self.health = self.health -1
 	Debug.Log("gate health: "..tostring(self.health))
 	if self.health <= 0 then
+		self.eventReceived = true
 		if String.EndsWith(GameEntityContextRequestBus.Broadcast.GetEntityName(self.entityId), "Left",0) then
 			Debug.Log("Hit: "..GameEntityContextRequestBus.Broadcast.GetEntityName(self.entityId)..". Opening Left Gate")
 			TransformBus.Event.RotateAroundLocalZ(self.entityId, 1.57)
--- Levels/main_level/Assets/Scripts/UnlockBar.lua
+++ Levels/main_level/Assets/Scripts/UnlockBar.lua
@@ -5,15 +5,19 @@ local unlockBar =
 function unlockBar:OnActivate()
 	self.ScriptEventHandler = HitSE.Connect(self, self.entityId)
 	self.health = 3
+	self.timeBetweenEvents = 0
+	self.tickHandler = TickBus:Connect(self)
+	self.eventReceived = false
 end
 
 function unlockBar:ReceiveHit(entityId, damage)
 	self.health = self.health -1
 	Debug.Log("wood bar health: "..tostring(self.health))
+	self.eventReceived = true
 	if self.health <= 0 then
 		TransformBus.Event.MoveEntity(self.entityId, Vector3(0,0,7))
 	end
-	
 end
+
 
 return unlockBar
